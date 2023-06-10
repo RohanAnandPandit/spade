e11666b854762a0f0b26311045f521b956eb4bf3
Rohan Pandit <rohananandpandit@gmail.com>
2023-06-10 21:16:02 +0100

    Fetch GeoJSON data for choropleth map using API
    - Re-render map when data changes by using unique key for component

--- frontend/src/components/charts/ChoroplethMap.tsx
+++ frontend/src/components/charts/ChoroplethMap.tsx
@@ -1,8 +1,11 @@
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 import { MapContainer, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { QueryResults, VariableCategories } from "../../types";
-import countries from "../../utils/countries.json";
+import { geoJSON, QueryResults, VariableCategories } from "../../types";
+import { getGeoJSON } from "../../api/queries";
+import { removePrefix } from "../../utils/queryResults";
+import { Spin } from "antd";
+import './ChoroplethMap.css';
 
 type ChoroplethMapProps = {
   results: QueryResults;
@@ -11,67 +14,70 @@ type ChoroplethMapProps = {
   height: number;
 };
 
-const ChoroplethMap = ({ results, variables, width, height }: ChoroplethMapProps) => {
+const ChoroplethMap = ({
+  results,
+  variables,
+  width,
+  height,
+}: ChoroplethMapProps) => {
   // const legendItems = [];
   // const legendItemsReverse = [...legendItems].reverse();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<geoJSON[]>([]);
 
-  const data = useMemo(() => {
-    const geoColumn = variables.geographical[0] ?? variables.key[0];
+  useEffect(() => {
+    setLoading(true);
+    const geoColumn =
+      variables.geographical[0] ?? (variables.key[0] || variables.lexical[0]);
+
     const geoIndex = results.header.indexOf(geoColumn);
     const valueColumn = variables.scalar[0];
     const valueIndex = results.header.indexOf(valueColumn);
-    const { features } = countries as any;
-    const geoValue = {};
 
     let minValue = Number.MAX_SAFE_INTEGER;
     let maxValue = Number.MIN_SAFE_INTEGER;
     for (let row of results.data) {
-      const location = row[geoIndex];
       const value = parseFloat(row[valueIndex]);
       minValue = Math.min(minValue, value);
       maxValue = Math.max(maxValue, value);
-      geoValue[location] = value;
     }
 
-    return features.map((location: any) => {
-      const { ADMIN, ISO_A3 } = location.properties;
-      const value = geoValue[ADMIN] || geoValue[ISO_A3] || 0;
-      location.properties.value = value;
-      location.properties.text = value.toLocaleString();
-      location.properties.color = `rgba(255,0,0,${
-        (value - minValue) / (maxValue - minValue)
-      })`;
+    Promise.all(
+      results.data.map(async (row) => {
+        const name = removePrefix(row[geoIndex]);
+        const value = parseFloat(row[valueIndex]);
+        const { data } = await getGeoJSON(name);
 
-      return location;
+        const location = data;
+        if (!location) return null;
+        // console.log(name);
+        location.properties.value = value;
+        location.properties.text = value.toLocaleString();
+        location.properties.color = `rgba(255,0,0,${
+          (value - minValue) / (maxValue - minValue)
+        })`;
+        return location;
+      })
+    ).then((responses) => {
+      setData(responses.filter((location) => location !== null) as geoJSON[]);
+      setLoading(false);
     });
-  }, [results.data, results.header, variables.geographical, variables.key, variables.scalar]);
+  }, [
+    results.data,
+    results.header,
+    variables.geographical,
+    variables.key,
+    variables.lexical,
+    variables.scalar,
+  ]);
 
   return (
-    <div>
-      {data.length === 0 ? (
-        <Loading />
-      ) : (
-        <div>
-          <WorldMap countries={data} width={width - 50} height={height} />
-          {/* <Legend legendItems={legendItemsReverse} /> */}
-        </div>
-      )}
-    </div>
-  );
-};
-
-const Loading = () => {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      Loading...
-    </div>
+    <Spin spinning={loading}>
+      <div>
+        <WorldMap data={data} width={width - 50} height={height} />
+        {/* <Legend legendItems={legendItemsReverse} /> */}
+      </div>
+    </Spin>
   );
 };
 
@@ -105,7 +111,7 @@ const Loading = () => {
 //   );
 // };
 
-const WorldMap = ({ countries, width, height }) => {
+const WorldMap = ({ data, width, height }) => {
   const mapStyle = {
     fillColor: "white",
     weight: 1,
@@ -115,16 +121,23 @@ const WorldMap = ({ countries, width, height }) => {
 
   const onEachLocation = (country: any, layer: any) => {
     layer.options.fillColor = country.properties.color;
-    const name = country.properties.ADMIN;
+    const name = country.properties.NAME;
     const text = country.properties.text;
     layer.bindPopup(`${name} ${text}`);
   };
 
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    setKey((key) => key + 1);
+  }, [data]);
+
   return (
     <MapContainer style={{ width, height }} zoom={2} center={[20, 60]}>
       <GeoJSON
+        key={key}
         style={mapStyle}
-        data={countries}
+        data={data}
         onEachFeature={onEachLocation}
       />
     </MapContainer>
