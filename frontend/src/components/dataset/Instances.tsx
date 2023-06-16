38606c3a2b0ee23d2fbb38f329813e06922567ae
Rohan Pandit <rohananandpandit@gmail.com>
2023-06-16 13:54:28 +0100

    Add label for class Select drop-down
    Move Divider inside of Spin component

--- frontend/src/components/dataset/Instances.tsx
+++ frontend/src/components/dataset/Instances.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import { PropertyType, URI } from "../../types";
 import { getInstances, getAllTypes } from "../../api/dataset";
-import { Collapse, Divider, Select, Skeleton, Tooltip } from "antd";
+import {
+  Collapse,
+  Divider,
+  Select,
+  Skeleton,
+  Space,
+  Spin,
+  Tooltip,
+  Typography,
+} from "antd";
 import { removePrefix } from "../../utils/queryResults";
 import { PropertyValues } from "./DataProperties";
 import { useStore } from "../../stores/store";
 
 const Instances = ({ repository }) => {
   const username = useStore().authStore.username!;
-  
+
   const [allTypes, setAllTypes] = useState<URI[]>([]);
   const [type, setType] = useState<URI | null>(null);
   const [instances, setInstances] = useState<URI[]>([]);
@@ -22,34 +31,36 @@ const Instances = ({ repository }) => {
 
   return (
     <>
-      <Select
-        placeholder={"Select class"}
-        style={{ width: 200 }}
-        value={type}
-        showSearch
-        filterOption={(input, option) =>
-          (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
-        }
-        onChange={(value) => {
-          setLoading(true);
-          setType(value);
-          getInstances(repository, value, username).then((res) => {
-            setInstances(res);
-            setLoading(false);
-          });
-        }}
-        options={allTypes.map((t) => {
-          return {
-            label: removePrefix(t),
-            value: t,
-          };
-        })}
-      />
-
+      <Space.Compact direction="vertical" style={{ width: "100%" }}>
+        <Typography.Text style={{ fontSize: 15 }}>Select class</Typography.Text>
+        <Select
+          placeholder={"Enter class name"}
+          // style={{ width: 200 }}
+          value={type}
+          showSearch
+          filterOption={(input, option) =>
+            (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+          }
+          onChange={(value) => {
+            setLoading(true);
+            setType(value);
+            getInstances(repository, value, username).then((res) => {
+              setInstances(res);
+              setLoading(false);
+            });
+          }}
+          options={allTypes.map((t) => {
+            return {
+              label: removePrefix(t),
+              value: t,
+            };
+          })}
+        />
+      </Space.Compact>
       {type && (
-        <>
-          <Divider>{instances.length} results</Divider>
-          <Skeleton active loading={loading}>
+        <Spin spinning={loading}>
+          <Space direction="vertical" style={{ width: "100%" }}>
+            <Divider>{instances.length} results</Divider>
             <Collapse defaultActiveKey={["1"]} onChange={() => {}}>
               {instances.map((uri: URI, index) => (
                 <Collapse.Panel
@@ -64,8 +75,8 @@ const Instances = ({ repository }) => {
                 </Collapse.Panel>
               ))}
             </Collapse>
-          </Skeleton>
-        </>
+          </Space>
+        </Spin>
       )}
     </>
   );
