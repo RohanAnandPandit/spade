51bdbe439ee2eeac1c2dcf0e2863faa69ca34610
Rohan Pandit <rohananandpandit@gmail.com>
2023-06-18 07:37:29 +0100

    Add test case for country borders query

--- backend/tests/analyser_test.py
+++ backend/tests/analyser_test.py
@@ -1,6 +1,8 @@
 import unittest
 from dotenv import load_dotenv, find_dotenv
 import os
+
+from backend.analysis import query_analysis
 from backend.repository import RemoteRepository
 
 load_dotenv(find_dotenv())
@@ -8,21 +10,41 @@ TEST_ENDPOINT = os.environ.get('TEST_ENDPOINT')
 
 
 class QueryAnalyserTest(unittest.TestCase):
-    repository = RemoteRepository(name="test", endpoint=TEST_ENDPOINT)
+    repository = RemoteRepository(name="mondial", endpoint=TEST_ENDPOINT)
 
-    def (self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_country_borders(self):
+        query = '''
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX : <http://www.semwebtech.org/mondial/10/meta#>
+        
+        SELECT ?country1 ?country2 ?length
+        WHERE {
+          ?b rdf:type :Border ;
+             :isBorderOf ?c1 ;
+             :isBorderOf ?c2 ;
+             :length ?length .
+          
+          ?c1 rdf:type :Country ;
+              :name ?country1 ;
+              :encompassedByInfo ?en .
+                    
+          ?c2 rdf:type :Country ;
+              :name ?country2 ;
+              :encompassedByInfo ?en2 .
+        }'''
+        analysis = query_analysis(query=query, repository=self.repository)
+        variables = analysis['variables']
+        potential_charts = set(analysis['visualisations'])
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertEqual(analysis['pattern'],
+                         "Three classes linked by functional properties")
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        self.assertTrue('length' in variables['scalar'])
+        self.assertTrue('country1' in variables['lexical'])
+        self.assertTrue('country2' in variables['lexical'])
+        self.assertEqual(potential_charts,
+                         {'Sankey', 'Network', 'Chord Diagram', 'Heat Map'})
 
 
 if __name__ == '__main__':
