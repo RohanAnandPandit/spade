9f03ff719d5c5afb04c91bca8484bb1a0f65e8a2
Rohan Pandit <rohananandpandit@gmail.com>
2023-06-18 07:01:08 +0100

    Move semicolon inside tracking code string

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -18,8 +18,7 @@ root.render(
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "hlisx0523d")`}
-        ;
+        })(window, document, "clarity", "script", "hlisx0523d");`}
       </script>
       <title>Semantic Web Schema and Data Visualiser</title>
     </Helmet>
