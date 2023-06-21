112f3b4cd8e2a2cc74da74ebb7f97aee6b27db9a
Rohan Pandit <rohananandpandit@gmail.com>
2023-06-21 07:57:05 +0100

    New scatter chart component with Apex charts

--- frontend/src/components/charts/ScatterChart.tsx
+++ frontend/src/components/charts/ScatterChart.tsx
@@ -1,17 +1,7 @@
-import { QueryResults, VariableCategories } from "../../types";
+import { QueryResults, Row, VariableCategories } from "../../types";
 import { groupByColumn, removePrefix } from "../../utils/queryResults";
-import randomColor from "randomcolor";
-import { useStore } from "../../stores/store";
 import { observer } from "mobx-react-lite";
-import {
-  VictoryAxis,
-  VictoryChart,
-  VictoryLabel,
-  VictoryScatter,
-  VictoryTheme,
-  VictoryTooltip,
-  createContainer,
-} from "victory";
+import ReactApexChart from "react-apexcharts";
 import { useMemo } from "react";
 
 type ScatterChartProps = {
@@ -23,90 +13,68 @@ type ScatterChartProps = {
 
 const ScatterChart = observer(
   ({ results, width, height, variables }: ScatterChartProps) => {
-    const rootStore = useStore();
-    const settings = rootStore.settingsStore;
     const { header, data } = results;
     const keyIdx = header.indexOf(variables.key[0]);
     const xIdx = header.indexOf(variables.scalar[0]);
     const yIdx = header.indexOf(variables.scalar[1]);
-    const zIdx = header.indexOf(variables.scalar[2]);
 
-    const seriesData = useMemo(() => {
-      const series =
-        keyIdx !== -1 ? Object.values(groupByColumn(data, keyIdx)) : [data];
+    const series = useMemo(() => {
+      if (keyIdx === -1) {
+        return [
+          {
+            data: data.map((row: Row) => {
+              const x = parseFloat(row[xIdx]);
+              const y = parseFloat(row[yIdx]);
 
-      return series.map((rows) => {
-        const color = randomColor({
-          luminosity: settings.darkMode() ? "light" : "dark",
-        });
-        return rows.map((row) => {
-          let label = `${keyIdx >= 0 ? removePrefix(row[keyIdx]) : ""}
-                        ${results.header[xIdx]}: ${parseFloat(row[xIdx])}
-                        ${results.header[yIdx]}: ${parseFloat(
-            row[yIdx]
-          ).toLocaleString()}`;
-          if (zIdx !== -1) {
-            label += `
-            ${results.header[zIdx]}: ${parseFloat(row[zIdx]).toLocaleString()}`;
-          }
+              return [x, y];
+            }),
+          },
+        ];
+      }
+      const groups = groupByColumn(data, keyIdx);
 
-          return {
-            label,
-            x: parseFloat(row[xIdx]),
-            y: parseFloat(row[yIdx]),
-            z: parseFloat(row[zIdx]),
-            fill: color,
-          };
-        });
+      return Object.keys(groups).map((name: string) => {
+        const rows = groups[name];
+        return {
+          name: removePrefix(name),
+          data: rows.map((row: Row) => {
+            const x = parseFloat(row[xIdx]);
+            const y = parseFloat(row[yIdx]);
+
+            return [x, y];
+          }),
+        };
       });
-    }, [keyIdx, data, results.header, xIdx, yIdx, zIdx, settings]);
+    }, [keyIdx, data, xIdx, yIdx]);
 
-    const VictoryZoomVoronoiContainer: any = createContainer("zoom", "voronoi");
+    const options: any = {
+      chart: {
+        height,
+        type: "scatter",
+        zoom: {
+          enabled: true,
+          type: "xy",
+        },
+      },
+      xaxis: {
+        title: { text: header[xIdx] },
+
+        tickAmount: 10,
+        labels: {
+          formatter: function (val: any) {
+            return parseFloat(val).toFixed(1);
+          },
+        },
+      },
+      yaxis: {
+        title: { text: header[yIdx] },
+        tickAmount: 7,
+      },
+    };
 
     return (
       <div>
-        <VictoryChart
-          width={width}
-          height={height}
-          theme={VictoryTheme.material}
-          padding={{ bottom: 40, left: 100, right: 50, top: 10 }}
-          domainPadding={{ x: 10, y: 10 }}
-          containerComponent={
-            <VictoryZoomVoronoiContainer
-              width={width}
-              height={height}
-              responsive={false}
-              labels={({ datum }: any) => datum.label}
-            />
-          }
-        >
-          <VictoryAxis
-            label={results.header[xIdx]}
-            axisLabelComponent={<VictoryLabel dy={20} />}
-            style={{
-              tickLabels: { fontSize: 15, padding: 5 },
-            }}
-            fixLabelOverlap
-            domainPadding={{ x: [10, -10], y: 5 }}
-          />
-          <VictoryAxis
-            label={results.header[yIdx]}
-            style={{
-              tickLabels: { fontSize: 15, padding: 5 },
-            }}
-            dependentAxis
-            fixLabelOverlap
-            domainPadding={{ x: [10, -10], y: 5 }}
-          />
-          <VictoryScatter
-            bubbleProperty="z"
-            maxBubbleSize={20}
-            minBubbleSize={5}
-            labelComponent={<VictoryTooltip style={{ fontSize: 15 }} />}
-            style={{ data: { fill: ({ datum }) => datum.fill } }}
-            data={seriesData.flat(1)}
-          />
-        </VictoryChart>
+        <ReactApexChart options={options} series={series} type="scatter" />
       </div>
     );
   }
