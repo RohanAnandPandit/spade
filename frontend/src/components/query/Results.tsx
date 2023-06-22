13e2b4d50c43c178f0df9c19d6210afbeba3369d
Rohan Pandit <rohananandpandit@gmail.com>
2023-06-22 16:41:40 +0100

    Change "Show Prefix" to "Show full URI" in Results

--- frontend/src/components/query/Results.tsx
+++ frontend/src/components/query/Results.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Alert, Space, Switch, Table, Tooltip, Typography } from "antd";
+import {
+  Alert,
+  Divider,
+  Space,
+  Switch,
+  Table,
+  Tooltip,
+  Typography,
+} from "antd";
 import { QueryResults } from "../../types";
 import { displayText, removePrefix } from "../../utils/queryResults";
 import Fullscreen from "./Fullscreen";
@@ -21,7 +29,7 @@ const Results = observer(({ results, loading }: QueryResultsProps) => {
   const height = Math.floor(
     window.screen.height * (settings.fullScreen() ? 0.8 : 0.5)
   );
-  if (Object.keys(results).includes('boolean')) {
+  if (Object.keys(results).includes("boolean")) {
     return (
       <Alert
         style={{ fontSize: 30 }}
@@ -35,13 +43,14 @@ const Results = observer(({ results, loading }: QueryResultsProps) => {
     <Fullscreen>
       <Space>
         <Typography.Text strong>{data.length} results</Typography.Text>
+        <Divider type="vertical" />
         <Switch
           checked={showPrefix}
           onChange={(checked: boolean) => setShowPrefix(checked)}
           checkedChildren={<BiShow size={15} style={{ marginBottom: 1 }} />}
           unCheckedChildren={<BiHide size={15} style={{ marginBottom: 1 }} />}
         />
-        Show Prefix
+        Show full URI
       </Space>
       <Table
         loading={loading}
@@ -52,7 +61,9 @@ const Results = observer(({ results, loading }: QueryResultsProps) => {
         dataSource={data.map((row, index) => {
           const values: any = {};
           for (let i = 0; i < row.length; i++) {
-            values[header[i]] = displayText(showPrefix ? row[i] : removePrefix(row[i]));
+            values[header[i]] = displayText(
+              showPrefix ? row[i] : removePrefix(row[i])
+            );
           }
           values.key = `${index}`;
           return values;
